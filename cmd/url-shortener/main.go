a210f21cacdd91209e88e5ea3481016c47d141cb
zkatemor <ea.zababurina@gmail.com>
2023-11-13 00:04:08 +0300

    init

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+	// cfg := config.MustLoad()
+
+	// todo: logger - slog
+
+	// todo: storage - sqlite
+
+	// todo: router - chi, render
+
+	// todo: run server
+}
+
+func setupLogger(env string) {
+
+}
